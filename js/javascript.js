54a385d332c5aa2ed7281a0a69b69a5734078267
Zakariya Auwal <192784011+Dxaks@users.noreply.github.com>
2025-06-22 17:59:33 +0100

    Inserting DOM into the Game lOgic

--- js/javascript.js
+++ js/javascript.js
@@ -1,69 +1,76 @@
+// selecting the container and buttons
+const container = document.getElementById("container");
+const btn = document.querySelectorAll("button");
+const scoreContainer = document.getElementById("score_container");
+const player = document.querySelector(".player");
+const computer = document.querySelector(".computer");
+const result = document.getElementById("result");
 
-// create a function that randomly returns one of the three strings (rock, paper, scissors) //
+// declaring the scores and the play round increments outside the onclick event to prevent reset each time the user clicks the button
+let playerScore = 0;
+let compScore = 0;
+let playRound = 0;
 
-function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3) + 1;
+// adding a click event to the container
+container.addEventListener("click", (even) => {
 
+// exit if the click element is not a button
+  if(!even.target.matches("button")) return; 
+    
+  // checking if the clicked element is a button and also to get the user selection
+    function getUserSelection() {
+    const userChoice = even.target.textContent;
+    even.stopPropagation();
+    return userChoice.toLowerCase();
+}
+  
+// function to get a random selection for the computer
+    function getCompSelection() {
+    let randomNumber = Math.floor(Math.random() *3 ) +1;
+    
     if (randomNumber === 1) {
-        return 'rock';
+      return "rock";
     } else if (randomNumber === 2) {
-        return 'paper';
-    } else {
-        return 'scissors';
+      return "paper";
+    } else if (randomNumber === 3) {
+      return "scissors";
     }
 }
-
-// create a function that returns a human choice //
-
-function getHumanChoice() {
-    let userInput = prompt('Enter Rock, Paper or Scissors');
-    return userInput.toLowerCase();
-    }
-// create a variables that read and save the current score for each player //
-
-    let playerScore = 0;
-    let computerScore = 0;
-
-// create a function that plays a single round of rock, paper, scissors //
-
-function playRound() {
-    let computerChoice = getComputerChoice();
-    let humanChoice = getHumanChoice();
-
-    if (computerChoice === humanChoice) {
-        return `it is a tie, you both chooses ${humanChoice}.`
-    } else if (
-        (humanChoice === 'rock' && computerChoice === 'scissors') ||
-        (humanChoice === 'scissors' && computerChoice === 'paper') ||
-        (humanChoice === 'paper' && computerChoice === 'rock')
-    ) {
-        playerScore++;
-        return `You win. ${humanChoice} beats ${computerChoice}. 
-                Scores ${playerScore} : ${computerScore}`
+   
+    const userSlection = getUserSelection();
+    const compSelection = getCompSelection();
+    
+    // the game logic to determine the winner
+  function getplayRound() {
+  
+        if (compSelection === userSlection) {
+         result.textContent = `it's a tie. You both choose ${userSlection} for this round.`;
+        
+      } else if (
+        (userSlection === "rock" && compSelection === "scissors") || (userSlection === "paper" && compSelection === "rock") || (userSlection === "scissors" && compSelection === "paper")
+        )
+      { playerScore++
+        player.textContent = `Player Score: ${playerScore}`;
+         result.textContent = `You win this round! You chose ${userSlection} and the computer chose ${compSelection}.`;
+      }
+      else {compScore++;
+        computer.textContent = `Computer Score: ${compScore}`;
+         result.textContent = `You lose this round! You chose ${userSlection} and the computer chose ${compSelection}.`;
+      }
+      
     }
 
-    else {
-        computerScore++;
-        return `You lose. ${computerChoice} beats ${humanChoice}.
-                Scores ${playerScore} : ${computerScore}`
-    }
-}
+    playRound++;
+    getplayRound();
 
-// create a function that plays 5 rounds of rock, paper, scissors //
-
-function playGame() {
-    for (let i = 1; i <= 5; i++) {
-        console.log(`Round ${i}`);
-        console.log(playRound());
-    }
-
-    if (playerScore > computerScore) {
-        console.log(`You win the game! Final score: ${playerScore} : ${computerScore}`);
-    } else if (computerScore > playerScore) {
-        console.log(`You lose the game! Final score: ${playerScore} : ${computerScore}`);
-    } else {
-        console.log(`The game is a tie! Final score: ${playerScore} : ${computerScore}`);
-    }
-}
-
-playGame();
+    if (playerScore > compScore && playRound === 5) {
+        btn.forEach(b => b.disabled = true);
+        result.textContent = "You win the game! \n Refresh to play again";
+    } else if (compScore > playerScore && playRound === 5) {
+        btn.forEach(b => b.disabled = true);
+        result.textContent = "Computer wins the game! \n Refresh to play again";      
+    } else if (playRound === 5) {
+        btn.forEach(b => b.disabled = true);
+        result.textContent = "It's a tie! Refresh to play again";      
+    } 
+})
